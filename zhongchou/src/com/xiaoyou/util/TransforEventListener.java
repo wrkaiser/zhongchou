e3ab61136205d92808e2ef5d46e8841c98fa03ad
wrkaiser <wrkaiser@163.com>
2016-03-14 13:39:09 +0800

    删除部分信息

--- zhongchou/src/com/xiaoyou/util/TransforEventListener.java
+++
@@ -1,9 +0,0 @@
-package com.xiaoyou.util;
-import java.util.EventListener;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface TransforEventListener extends EventListener{
-	void update(HttpServletRequest request,HttpServletResponse response,String to,long send,long size ) ;  
-}
